f16f6d5c5927b9cc4ecf828b4fce669d4b2a72a5
willsher1 <94994801+willsher1@users.noreply.github.com>
2025-04-13 22:33:31 -0700

    Change theme

---
+++ src/mkdocs_test/main.py
@@ -0,0 +1,10 @@
+def greet(name: str) -> str:
+    """Return a customized greeting.
+
+    Args:
+        name: The name of the person to be greeted.
+
+    Returns:
+        A greeting appropriate to the given name
+    """
+    return f"Hello, {name}"
